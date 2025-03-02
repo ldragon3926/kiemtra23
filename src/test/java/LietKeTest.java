bf24fb1b1d69d8aaeffe27598cb17496cebb6388
LDragon <ldragon3919@gmail.com>
2025-03-02 15:07:52 +0700

    kiemtra2/3

--- src/test/java/LietKeTest.java
+++ src/test/java/LietKeTest.java
@@ -24,4 +24,11 @@ public class LietKeTest {
     public void testLK5() {
         Assertions.assertThrows(IllegalArgumentException.class, () -> lietke.printTu1Den100ex(-100));    }
 
+    @Test
+    public void testLK6() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> lietke.printTu1Den100ex(-1));    }
+    @Test
+    public void testLK7() {
+        Assertions.assertEquals("1 2 3 4 6 7 8 9 11 12 13 14 16 17 18 19 21 22 23 24 26 27 28 29 31 32 33 34 36 37 38 39 41 42 43 44 46 47 48 49 51 52 53 54 56 57 58 59 61 62 63 64 66 67 68 69 71 72 73 74 76 77 78 79 81 82 83 84 86 87 88 89 91 92 93 94 96 97 98 99 ",  lietke.printTu1Den100ex(1));
+    }
 }
